faea4a2c0031d5829c95d72d0ea54bc819ebf1f9
Chris Casinghino <ccasinghino@draper.com>
2018-12-27 13:50:19 -0500

    Fix circular import bug and clean up formatting

--- src/Symbols.hs
+++ src/Symbols.hs
@@ -25,47 +25,44 @@
  -}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DataKinds #-}
+
 module Symbols where
 
-import Data.Maybe
-import Data.Either
-import Data.List (intercalate)
 import AST
 import CommonFn
+import Data.Either
+import Data.List (intercalate, nub)
+import Data.Maybe
 import ErrorMsg
 
 -- The Symbol module creates two data structures with symbols for the parsed dpl
 -- files and provides a set of functions for querrying
-
 -- Symbols Data Structures --
-
 -- Module Symbols - An association list of full symbol tables for each parsed dpl module
 -- Symbols could be a superset of what is used by a given policy
 type ModSymbols = [(ModName, SymbolTable QSym)]
+
 -- UsedSymbols gives the actual list of Symbols used by the policy being processed
 -- This type is actually constructed in the Validate module see: validateMain Fn
 type UsedSymbols = [(ModName, QSym)]
 
 -- SymbolTable is split up by type of symbol
-data SymbolTable n = SymbolTable  { modDecl       :: ModuleDecl QSym
-                                  , importSyms    :: [ModName]
-                                  , typeSyms      :: [(QSym, TypeDecl n)]
-                                  , tagSyms       :: [(QSym, TagDecl n)]
-                                  , policySyms    :: [(QSym, PolicyDecl n)]
-                                  , groupSyms     :: [(QSym, GroupDecl [ISA] n)]
-                                  , requires      :: [RequireDecl n]
-                                  }
-  deriving Show
+data SymbolTable n = SymbolTable
+  { modDecl :: ModuleDecl QSym
+  , importSyms :: [ModName]
+  , typeSyms :: [(QSym, TypeDecl n)]
+  , tagSyms :: [(QSym, TagDecl n)]
+  , policySyms :: [(QSym, PolicyDecl n)]
+  , groupSyms :: [(QSym, GroupDecl [ISA] n)]
+  , requires :: [RequireDecl n]
+  } deriving (Show)
 
 -- Symbols API --
-
 -- take a list of parsed module declarations and produce the symbol tables
 buildSymbolTables :: [ModuleDecl QSym] -> Either [ErrMsg] ModSymbols
 buildSymbolTables mods = symbolTables mods
 
-
 -- Use these functions when the thing you are looking for may not exist
-
 -- Lookup Fns return Either a Decl or an error message
 type LookupResult a = Either ErrMsg (ModName, a)
 
@@ -87,13 +84,13 @@ lookupTag ms mn qs = lookupQSym ms tagSyms mn qs
 lookupPolicy :: ModSymbols -> ModName -> QSym -> LookupResult (PolicyDecl QSym)
 lookupPolicy ms mn qs = lookupQSym ms policySyms mn qs
 
-lookupGroup :: ModSymbols -> ModName -> QSym -> LookupResult (GroupDecl [ISA] QSym)
+lookupGroup ::
+     ModSymbols -> ModName -> QSym -> LookupResult (GroupDecl [ISA] QSym)
 lookupGroup ms mn qs = lookupQSym ms groupSyms mn qs
 
 -- Use these functions when you are sure the thing you are looking for exists
 -- andthe symbol has the correct type.
 -- If it doesn't we call error with a message to terminate the program
-
 getType :: ModSymbols -> ModName -> QSym -> (ModName, TypeDecl QSym)
 getType ms mn qs = mayErr "Failed Type Lookup" $ lookupQSym ms typeSyms mn qs
 
@@ -101,15 +98,14 @@ getTag :: ModSymbols -> ModName -> QSym -> (ModName, TagDecl QSym)
 getTag ms mn qs = mayErr "Failed Tag Lookup" $ lookupQSym ms tagSyms mn qs
 
 getPolicy :: ModSymbols -> ModName -> QSym -> (ModName, PolicyDecl QSym)
-getPolicy ms mn qs = mayErr "Failed Policy Lookup" $ lookupQSym ms policySyms mn qs
+getPolicy ms mn qs =
+  mayErr "Failed Policy Lookup" $ lookupQSym ms policySyms mn qs
 
 getGroup :: ModSymbols -> ModName -> QSym -> (ModName, GroupDecl [ISA] QSym)
 getGroup ms mn qs = mayErr "Failed Group Lookup" $ lookupQSym ms groupSyms mn qs
 
-
 -- Use these Fns to filter the list of UsedSymbols down to a particular type
 -- e.g. Tags, Types, Groups, Policies
-
 usedTypes :: UsedSymbols -> UsedSymbols
 usedTypes us = filter isType us
   where
@@ -133,15 +129,14 @@ usedRequires ms us = concatMap reqs mods
   where
     mods :: [ModName]
     mods = nubSort $ map fst us
-    reqs mn = case lookup mn ms of
-                           Nothing -> []
-                           Just st -> zip (repeat mn) (requires st)
+    reqs mn =
+      case lookup mn ms of
+        Nothing -> []
+        Just st -> zip (repeat mn) (requires st)
 
 -- Use these functions when you are sure the thing you are looking for exists
 -- andthe symbol has the correct type.
 -- If it doesn't we call error with a message to terminate the program
-
-                           
 typeDecl :: ModSymbols -> (ModName, QSym) -> (ModName, TypeDecl QSym)
 typeDecl ms (mn, qt@(QType _)) = getType ms mn qt
 
@@ -156,47 +151,78 @@ resolveQSym ms startMod qs = qualifyQSym (moduleForQSym ms startMod qs) qs
 
 -- Lookup a qsym to find the module its declared in
 moduleForQSym :: ModSymbols -> ModName -> QSym -> ModName
-moduleForQSym ms mn qs@(QType _)    = fst $ mayErr "Failed module lookup" $ lookupQSym ms typeSyms mn qs
-moduleForQSym ms mn qs@(QTag _)     = fst $ mayErr "Failed module lookup" $ lookupQSym ms tagSyms mn qs
-moduleForQSym ms mn qs@(QPolicy _)  = fst $ mayErr "Failed module lookup" $ lookupQSym ms policySyms mn qs
-moduleForQSym ms mn qs@(QGroup _)   = fst $ mayErr "Failed module lookup" $ lookupQSym ms groupSyms mn qs
-moduleForQSym ms mn qs@(QVar _)   = mn
-
+moduleForQSym ms mn qs@(QType _) =
+  fst $ mayErr "Failed module lookup" $ lookupQSym ms typeSyms mn qs
+moduleForQSym ms mn qs@(QTag _) =
+  fst $ mayErr "Failed module lookup" $ lookupQSym ms tagSyms mn qs
+moduleForQSym ms mn qs@(QPolicy _) =
+  fst $ mayErr "Failed module lookup" $ lookupQSym ms policySyms mn qs
+moduleForQSym ms mn qs@(QGroup _) =
+  fst $ mayErr "Failed module lookup" $ lookupQSym ms groupSyms mn qs
+moduleForQSym ms mn qs@(QVar _) = mn
+  -- look up a qsym by finding the ST for its module and then using fn to select which Syms to lu
 
 -- Internal helper functions....
-
-  -- look up a qsym by finding the ST for its module and then using fn to select which Syms to lu
-lookupQSym :: ModSymbols -> (SymbolTable QSym -> [(QSym,a)]) -> ModName -> QSym -> LookupResult a
-lookupQSym sts fn mn qs | isQualified qs = case lookup (modName qs) $ catMaybes $ lookupMod sts fn qs mn of
-                            Nothing -> Left $ "Unable to find " ++ (qualSymStr qs)
-                            Just a -> Right (modName qs, a)
-lookupQSym sts fn mn qs = case catMaybes $ lookupMod sts fn qs mn of
-                            [] -> Left $ "Unable to find symbol " ++ (unqualSymStr qs) ++
-                              " in modules: " ++ (intercalate ", " $ map (dotName.fst) sts) ++
-                              " reached from: " ++ (dotName mn)
-                            res:[] -> Right res
-                            reses -> Left $ "Multiple symbols found: " ++ (unqualSymStr qs) ++
-                              " reached from: " ++ (dotName mn) ++
-                              " from modules: " ++ (intercalate ", " $ map (dotName.fst) reses)
+lookupQSym ::
+     Eq a
+  => ModSymbols
+  -> (SymbolTable QSym -> [(QSym, a)])
+  -> ModName
+  -> QSym
+  -> LookupResult a
+lookupQSym sts fn mn qs
+  | isQualified qs =
+    case lookup (modName qs) $ catMaybes $ lookupMod sts fn qs mn of
+      Nothing -> Left $ "Unable to find " ++ (qualSymStr qs)
+      Just a -> Right (modName qs, a)
+lookupQSym sts fn mn qs =
+  case nub $ catMaybes $ lookupMod sts fn qs mn of
+    [] ->
+      Left $
+      "Unable to find symbol " ++
+      (unqualSymStr qs) ++
+      " in modules: " ++
+      (intercalate ", " $ map (dotName . fst) sts) ++
+      " reached from: " ++ (dotName mn)
+    res:[] -> Right res
+    reses ->
+      Left $
+      "Multiple symbols found: " ++
+      (unqualSymStr qs) ++
+      " reached from: " ++
+      (dotName mn) ++
+      " from modules: " ++ (intercalate ", " $ map (dotName . fst) reses)
 
 type PartialResult a = Maybe (ModName, a)
 
-lookupMod :: ModSymbols -> (SymbolTable  QSym -> [(QSym,a)]) -> QSym -> ModName -> [PartialResult a]
-lookupMod sts fn qs mn = case lookup mn sts of
-                           Nothing -> []
-                           Just st -> lookupSym sts st fn mn qs
+lookupMod ::
+     ModSymbols
+  -> (SymbolTable QSym -> [(QSym, a)])
+  -> QSym
+  -> ModName
+  -> [PartialResult a]
+lookupMod sts fn qs mn =
+  case lookup mn sts of
+    Nothing -> []
+    Just st -> lookupSym sts st fn mn qs
 
 -- look up a qsym in a module using fn to select which type of Syms to lu
-lookupSym :: ModSymbols -> SymbolTable QSym -> (SymbolTable  QSym -> [(QSym,a)]) -> ModName -> QSym -> [PartialResult a]
-lookupSym sts st fn mn qs = case lookup qs $ fn st of
-                              Nothing -> importedSyms fn
-                              Just a -> [Just (mn, a)] ++ importedSyms fn
+lookupSym ::
+     ModSymbols
+  -> SymbolTable QSym
+  -> (SymbolTable QSym -> [(QSym, a)])
+  -> ModName
+  -> QSym
+  -> [PartialResult a]
+lookupSym sts st fn mn qs =
+  case lookup qs $ fn st of
+    Nothing -> importedSyms fn
+    Just a -> [Just (mn, a)] ++ importedSyms fn
   where
     imports :: [ModName]
     imports = importSyms st
-    importedSyms :: (SymbolTable  QSym -> [(QSym,a)]) -> [PartialResult a]
+    importedSyms :: (SymbolTable QSym -> [(QSym, a)]) -> [PartialResult a]
     importedSyms fn = concatMap (lookupMod sts fn qs) imports
-  
 
 -- This computes all the tags declared in the "tags" section of a module.  In
 -- the future world where we allow modules to examine but not modify tags from
@@ -205,11 +231,12 @@ moduleTags :: ModSymbols -> UsedSymbols -> ModName -> [TagDecl QSym]
 moduleTags ms allSyms mn = map toTagDecl $ filter byTagDecl allSyms
   where
     byTagDecl :: (ModName, QSym) -> Bool
-    byTagDecl (mn', QTag _) | mn == mn' = True
+    byTagDecl (mn', QTag _)
+      | mn == mn' = True
     byTagDecl _ = False
     toTagDecl :: (ModName, QSym) -> TagDecl QSym
     toTagDecl (modN, qs) = snd $ getTag ms modN qs
-    
+
 {-
 -- Returns all type declarations
 allTypes :: ModSymbols -> [TypeDecl QSym]
@@ -225,8 +252,6 @@ allTags ms = map snd tags
 --  where
 --    pn = unqualQSym qpn
   -}
-
-  
 -- create the set of symbol tables for an include graph
 symbolTables :: [ModuleDecl QSym] -> Either [ErrMsg] ModSymbols
 symbolTables mods = Right $ fmap symbolTable mods
@@ -235,17 +260,18 @@ symbolTables mods = Right $ fmap symbolTable mods
 symbolTable :: ModuleDecl QSym -> (ModName, SymbolTable QSym)
 symbolTable m@(ModuleDecl _ mn sects) = (mn, mkST)
   where
-    mkST = SymbolTable { modDecl       = m
-                       , importSyms    = map importDeclName (sect importS sects)
-                       , typeSyms      = map (declareSym mn) (sect typeS sects)
-                       , tagSyms       = map (declareSym mn) (sect tagS sects)
-                       , policySyms    = map (declareSym mn) (sect policieS sects)
-                       , groupSyms     = map (declareSym mn) (sect groupS sects)
-                       , requires       = (sect requireS sects)
-                       }
+    mkST =
+      SymbolTable
+        { modDecl = m
+        , importSyms = map importDeclName (sect importS sects)
+        , typeSyms = map (declareSym mn) (sect typeS sects)
+        , tagSyms = map (declareSym mn) (sect tagS sects)
+        , policySyms = map (declareSym mn) (sect policieS sects)
+        , groupSyms = map (declareSym mn) (sect groupS sects)
+        , requires = (sect requireS sects)
+        }
     declareSym :: Symbol s => ModName -> s -> (QSym, s)
     declareSym _ s = (unqualQSym $ qsym s, s)
-    
 {-
 -- check if the symbol table has errors
 symbolTableErrors :: SymbolTable ErrQSym -> [ErrMsg]
