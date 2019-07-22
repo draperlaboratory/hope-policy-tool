753bdeaa2debc33c7cea3e264ba34c79fd027b2d
Amanda Strnad <astrnad@draper.com>
2019-07-22 14:20:14 -0400

    Remove pass variable from Haskell too

--- src/GenRuleC.hs
+++ src/GenRuleC.hs
@@ -667,7 +667,6 @@ policyEval :: Bool -> Bool -> ModSymbols -> OpGroupMap
 policyEval debug _profile ms ogMap tagInfo (modN, pd@(PolicyDecl _ _ _ pEx)) =
   [cedecl|
        int $id:(singlePolicyEvalName pd) ($params:policyInputParams) {
-         int $id:resultVar = $id:policyIFailName;
 
          $stm:body
 
@@ -675,13 +674,10 @@ policyEval debug _profile ms ogMap tagInfo (modN, pd@(PolicyDecl _ _ _ pEx)) =
          return $id:policyIFailName;
        } |]
   where
-   resultVar :: String
-   resultVar = "policyResult"
-
    body :: Stm
    body = Block p' noLoc
      where
-       p' = translatePolicy debug ms ogMap pd tagInfo resultVar modN pEx
+       p' = translatePolicy debug ms ogMap pd tagInfo modN pEx
 
 -- Takes as arguments:
 --  - whether to print debug info
@@ -689,7 +685,6 @@ policyEval debug _profile ms ogMap tagInfo (modN, pd@(PolicyDecl _ _ _ pEx)) =
 --  - Info about opgroups
 --  - the policy declaration (used for policy name)
 --  - the tag encoding list
---  - a variable name x where the result should be stored
 --  - The enclosing module name
 --  - the policy itself.
 --
@@ -699,25 +694,25 @@ policyEval debug _profile ms ogMap tagInfo (modN, pd@(PolicyDecl _ _ _ pEx)) =
 -- policyEFailName.  In the first case, the function will add the appropriate
 -- tags from this policy to the array of result tag lists.
 translatePolicy :: Bool -> ModSymbols -> OpGroupMap ->  PolicyDecl QSym
-                -> TagInfo -> String -> ModName
+                -> TagInfo -> ModName
                 -> PolicyEx QSym -> [BlockItem]
-translatePolicy dbg ms ogMap pd tagInfo pass modN (PEVar _ x) =
+translatePolicy dbg ms ogMap pd tagInfo modN (PEVar _ x) =
   let (modN', (PolicyDecl _ _ _ p)) = getPolicy ms modN x in
-    translatePolicy dbg ms ogMap pd tagInfo pass modN' p
-translatePolicy dbg ms ogMap pd tagInfo pass modN (PECompExclusive _ p1 p2) =
+    translatePolicy dbg ms ogMap pd tagInfo modN' p
+translatePolicy dbg ms ogMap pd tagInfo modN (PECompExclusive _ p1 p2) =
   [citems|$items:p1';
           $items:p2'
           |]
   where
-     p1' = translatePolicy dbg ms ogMap pd tagInfo pass modN p1
-     p2' = translatePolicy dbg ms ogMap pd tagInfo pass modN p2
-translatePolicy dbg ms ogMap pd tagInfo pass modN (PECompPriority l p1 p2) =
-  translatePolicy dbg ms ogMap pd tagInfo pass modN (PECompExclusive l p1 p2)
-translatePolicy _ _ _ _ _ _pass _ (PENoChecks _) =
+     p1' = translatePolicy dbg ms ogMap pd tagInfo modN p1
+     p2' = translatePolicy dbg ms ogMap pd tagInfo modN p2
+translatePolicy dbg ms ogMap pd tagInfo modN (PECompPriority l p1 p2) =
+  translatePolicy dbg ms ogMap pd tagInfo modN (PECompExclusive l p1 p2)
+translatePolicy _ _ _ _ _ _ (PENoChecks _) =
   [citems| return $id:policySuccessName;|]
-translatePolicy _ _ _ _ _ _ _ (PECompModule _ _p1 _p2) =
+translatePolicy _ _ _ _ _ _ (PECompModule _ _p1 _p2) =
   error "Unsupported: PECompModule in translatePolicy"
-translatePolicy dbg ms ogMap pd tagInfo _pass modN (PERule _ rc@(RuleClause _ ogrp rpat rres)) =
+translatePolicy dbg ms ogMap pd tagInfo modN (PERule _ rc@(RuleClause _ ogrp rpat rres)) =
   [citems|
        if(ms_contains($id:ciArgName,$id:(tagName (qualifiedOpGrpMacro)))) {
          int $id:matchVar = $exp:patExp;
@@ -922,7 +917,6 @@ translatePatterns ms mn mask tagInfo ogmap pats =
 --   - The name of the policy mask
 --   - An association list mapping operands to C macros based on the opgroup.
 --   - A mapping from policy variables to C expressions.
---   - The name of the result variable
 --   - The result to be translated.
 -- Results:
 --   - A series of statements.  These will have the result of returning
